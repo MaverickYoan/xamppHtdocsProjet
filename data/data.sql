3977e2747ec79fea5dc1bd2dfa40a817da7a1790
MaverickYoan <ensemblepourlafetelibre@gmail.com>
2025-06-14 06:42:37 +0200

    majFiles

---
+++ data/data.sql
@@ -0,0 +1,3 @@
+-- fichier vierge à remplacer par un éventuel export au format .sql
+
+-- Dans ce chapitre, nous allons voir nos premières requêtes SQL et on va commencer par les requêtes DDL (Data Definition Language) qui permettent de définir la structure des données. On va commencer par la requête CREATE TABLE qui permet de créer une table et de définir l'ensemble des colonnes qui la compose.
